a4f2e7522186e52df2eaa6404ddd8aa9ea7c7a17
Abidzarzlfr <abidzarbijeh@gmail.com>
2024-03-25 03:52:21 +0700

    feat(login, middleware): feat example login page and middleware

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,15 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('beranda');
+// });
+
+Route::group(['middleware' => 'guest'], function () {
+    Route::get('/', function () {
+        return view('beranda');
+    });
+    Route::get('/login', function () {
+        return view('auth.login');
+    });
 });
